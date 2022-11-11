e4485887ac8c871739e17d8ba18ec1dc42f30dfc
Andre Barbosa <andrebarbosadg@gmail.com>
2022-11-11 01:08:58 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/andrebarbosa/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andrebarbosa/cursomc/resources/CategoriaResource.java
@@ -1,20 +1,33 @@
 package com.andrebarbosa.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.andrebarbosa.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 	@GetMapping(value="/1")
-	public String listarUm() {
-		return "Listando apenas um!";
+	public Object listarUm() {
+		return listar().get(0);
 	}
 }
