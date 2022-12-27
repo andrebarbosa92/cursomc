7b24f157c4232fc06f90d98ff9b3b239bacf86f4
Andre Barbosa <andrebarbosadg@gmail.com>
2022-12-27 16:21:37 -0300

    Estado e cidade

---
+++ src/main/java/com/andrebarbosa/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.andrebarbosa.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andrebarbosa.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
